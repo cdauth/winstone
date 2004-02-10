4018262f51eee46eb12a1d0880f987c35bc060bd
Rick Knowles <rick_knowles@hotmail.com>
2004-02-10 14:06:30 +0000

    remainder after broken pipe

---
+++ development/src/com/rickknowles/winstone/auth/BasicAuthenticationHandler.java
@@ -0,0 +1,83 @@
+/*
+ * Winstone Servlet Container
+ * Copyright (C) 2003 Rick Knowles
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * Version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License Version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * Version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.rickknowles.winstone.auth;
+
+import com.rickknowles.winstone.*;
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Handles HTTP basic authentication.
+ *
+ * @author mailto: <a href="rick_knowles@hotmail.com">Rick Knowles</a>
+ * @version $Id$
+ */
+public class BasicAuthenticationHandler extends AuthenticationHandler
+{
+  public BasicAuthenticationHandler(List roles, String realmName,
+    WinstoneResourceBundle resources, SecurityConstraint constraints[],
+    AuthenticationRealm realm)
+  {
+    super(realm, constraints, resources, realmName);
+    Logger.log(Logger.FULL_DEBUG, resources.getString("BasicAuthenticationHandler.Initialised",
+      "[#name]", realmName));
+  }
+
+  /**
+   * Call this once we know that we need to authenticate
+   */
+  protected void requestAuthentication(HttpServletRequest request,
+    HttpServletResponse response, String pathRequested) throws IOException
+  {
+    // Return unauthorized, and set the realm name
+    response.setHeader("WWW-Authenticate", "Basic Realm=\"" + this.realmName + "\"");
+    response.sendError(response.SC_UNAUTHORIZED,
+      this.resources.getString("BasicAuthenticationHandler.UnauthorizedMessage"));
+  }
+
+  /**
+   * Handling the (possible) response
+   */
+  protected void validatePossibleAuthenticationResponse(WinstoneRequest request,
+    WinstoneResponse response, String pathRequested) throws IOException
+  {
+    String authorization = request.getHeader("Authorization");
+    if (authorization == null)
+      return;
+
+    // Check it's basic
+    if (authorization.toLowerCase().startsWith("basic"))
+    {
+      String decoded = decodeBase64String(authorization.substring(5).trim());
+      int delimPos = decoded.indexOf(':');
+      if (delimPos != -1)
+      {
+        AuthenticationPrincipal principal = this.realm.authenticateByUsernamePassword(
+            decoded.substring(0, delimPos).trim(), decoded.substring(delimPos + 1).trim());
+        if (principal != null)
+        {
+          principal.setAuthType(request.BASIC_AUTH);
+          request.setRemoteUser(principal);
+        }
+      }
+    }
+  }
+}
+
---
+++ development/src/com/rickknowles/winstone/realm/ArgumentsRealm.java
@@ -0,0 +1,89 @@
+/*
+ * Winstone Servlet Container
+ * Copyright (C) 2003 Rick Knowles
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * Version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License Version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * Version 2 along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.rickknowles.winstone.realm;
+
+import java.util.*;
+import java.security.Principal;
+import com.rickknowles.winstone.*;
+
+/**
+ * Base class for authentication realms. Subclasses provide the source of
+ * authentication roles, usernames, passwords, etc, and when asked for
+ * validation respond with a role if valid, or null otherwise.
+ *
+ * @author mailto: <a href="rick_knowles@hotmail.com">Rick Knowles</a>
+ * @version $Id$
+ */
+public class ArgumentsRealm extends AuthenticationRealm
+{
+  final String PASSWORD_PREFIX = "argumentsRealm.passwd.";
+  final String ROLES_PREFIX    = "argumentsRealm.roles.";
+
+  private Map passwords;
+  private Map roles;
+
+  /**
+   * Constructor - this sets up an authentication realm, using the arguments supplied
+   * on the command line as a source of userNames/passwords/roles.
+   */
+  public ArgumentsRealm(WinstoneResourceBundle resources, Map args)
+  {
+    super(resources, args);
+    this.passwords = new Hashtable();
+    this.roles = new Hashtable();
+    
+    for (Iterator i = args.keySet().iterator(); i.hasNext(); )
+    {
+      String key = (String) i.next();
+      if (key.startsWith(PASSWORD_PREFIX))
+      {
+        String userName = key.substring(PASSWORD_PREFIX.length());
+        String password = (String) args.get(key);
+        this.passwords.put(userName, password);
+
+        String roleList = (String) args.get(ROLES_PREFIX + userName);
+        StringTokenizer st = new StringTokenizer(roleList, ",");
+        String roleArray[] = new String[st.countTokens()];
+        for (int n = 0; n < roleArray.length; n++)
+          roleArray[n] = st.nextToken();
+        Arrays.sort(roleArray);
+        this.roles.put(userName, roleArray);
+      }
+    }
+
+    Logger.log(Logger.FULL_DEBUG, resources.getString("ArgumentsRealm.Initialised",
+      "[#userCount]", "" + this.passwords.size()));
+  }
+
+  /**
+   * Authenticate the user - do we know them ? Return a principal once we know them
+   */
+  public AuthenticationPrincipal authenticateByUsernamePassword(String userName, String password)
+  {
+    String realPassword = (String) this.passwords.get(userName);
+    if (realPassword == null)
+      return null;
+    else if (!realPassword.equals(password))
+      return null;
+    else
+      return new AuthenticationPrincipal(userName, password,
+                                          (String []) this.roles.get(userName));
+  }
+}
+
+
