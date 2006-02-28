2467e2019893b4ed60d4b63aba6acd77cfe0206c
Rick Knowles <rick_knowles@hotmail.com>
2006-02-28 07:32:46 +0000

    Changed license conditions from GPL to CDDL/LGPL

--- src/java/winstone/jndi/java/javaURLContextFactory.java
+++ src/java/winstone/jndi/java/javaURLContextFactory.java
@@ -1,19 +1,8 @@
 /*
- * Winstone Servlet Container
- * Copyright (C) 2003 Rick Knowles
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * Version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License Version 2 for more details.
- *
- * You should have received a copy of the GNU General Public License
- * Version 2 along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Copyright 2003-2006 Rick Knowles <winstone-devel at lists sourceforge net>
+ * Distributed under the terms of either:
+ * - the common development and distribution license (CDDL), v1.0; or
+ * - the GNU Lesser General Public License, v2.1 or later
  */
 package winstone.jndi.java;
 
