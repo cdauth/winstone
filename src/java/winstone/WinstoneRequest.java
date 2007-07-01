52afa4f51fac6d13a1ea1390ad8b1925fedbc206
Rick Knowles <rick_knowles@hotmail.com>
2007-07-01 04:00:46 +0000

    shouldn't have the context path in the prefix either

--- src/java/winstone/WinstoneRequest.java
+++ src/java/winstone/WinstoneRequest.java
@@ -1210,7 +1210,6 @@ public class WinstoneRequest implements HttpServletRequest {
         if (!((getServerPort() == 80) && getScheme().equals("http"))
                 && !((getServerPort() == 443) && getScheme().equals("https")))
             url.append(':').append(getServerPort());
-        url.append(this.webappConfig.getContextPath());
         url.append(getRequestURI()); // need encoded form, so can't use servlet path + path info
         return url;
     }
